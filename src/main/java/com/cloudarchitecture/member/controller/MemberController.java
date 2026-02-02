d25d4894582803db2d3ee4a111c391413492b320
hyu1335 <hyuham1335@gmail.com>
2026-02-02 11:56:33 +0900

    feat: 요청 발생시 로그메세지 출력 추가, 상태 모니터링 기능 추가

--- src/main/java/com/cloudarchitecture/member/controller/MemberController.java
+++ src/main/java/com/cloudarchitecture/member/controller/MemberController.java
@@ -6,23 +6,27 @@ import com.cloudarchitecture.member.dto.response.GetOneMemberResponse;
 import com.cloudarchitecture.member.service.MemberService;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class MemberController {
 
     private final MemberService memberService;
 
     @PostMapping("/api/members")
     public ResponseEntity<CreateMemberResponse> createMember(@RequestBody CreateMemberRequest request) {
+        log.info("[API - LOG] 멤버 생성 요청");
         return ResponseEntity.status(HttpStatus.CREATED).body(memberService.createMember(request));
     }
 
     @GetMapping("/api/members/{memberId}")
-    public ResponseEntity<GetOneMemberResponse> getOneMember(@PathVariable Long memberId){
+    public ResponseEntity<GetOneMemberResponse> getOneMember(@PathVariable Long memberId) {
+        log.info("[API - LOG] 멤버 조회 요청");
         return ResponseEntity.status(HttpStatus.OK).body(memberService.getOneMember(memberId));
     }
 }
